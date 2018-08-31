325da3ebeba249d2c1b06e2eab9b482d8f2d4a71
Bala Krishna Ragala <baluragala@gmail.com>
2018-08-31 15:30:01 +0530

    css loader

--- react_scratch/src/App.js
+++ react_scratch/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to React 16</h1>
+        <h1>Welcome to React 16.3</h1>
       </div>
     );
   }
