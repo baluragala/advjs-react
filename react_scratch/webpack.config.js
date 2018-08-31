5c5a671278b6a2e2cfc50b21959d545a6ff926fb
Bala Krishna Ragala <baluragala@gmail.com>
2018-08-31 16:53:10 +0530

    ssr

--- react_scratch/webpack.config.js
+++ react_scratch/webpack.config.js
@@ -1,23 +1,55 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
 });
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        loader: ["style-loader", "css-loader"]
-      }
-    ]
+module.exports = [
+  {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          loader: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    plugins: [htmlPlugin]
   },
-  plugins: [htmlPlugin]
-};
+  {
+    entry: "./src/server.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "server.js"
+    },
+    target: "node",
+    node: {
+      console: false,
+      global: false,
+      process: false,
+      Buffer: false,
+      __filename: false,
+      __dirname: false
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          loader: ["style-loader", "css-loader"]
+        }
+      ]
+    }
+  }
+];
